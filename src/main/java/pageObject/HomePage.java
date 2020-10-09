3dbc94b50b1541f4d7449badc1c56ac910c083ee
Punam <punampalo2@gmail.com>
2020-10-09 17:15:11 +0200

    changes in homepageobject

--- src/main/java/pageObject/HomePage.java
+++ src/main/java/pageObject/HomePage.java
@@ -11,21 +11,14 @@ public class HomePage {
 	public WebDriver driver;
 	
 	
-	//By menuLink = By.id("nav-hamburger-menu");
-	//By selectBooks = By.xpath("//body/div[@id='hmenu-container']/div[@id='hmenu-canvas']/div[@id='hmenu-content']/ul[1]/li[6]/a[1]");
-	//By selectAllBookssubmenu = By.xpath("//body/div[@id='hmenu-container']/div[@id='hmenu-canvas']/div[@id='hmenu-content']/ul[6]/li[3]/a[1]");
-	//By searchTextBox = By.id("twotabsearchtextbox");
-	//By selectBestsellerItems = By.xpath("//body/div[@id='a-page']/div[4]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[2]/div[3]");
-	//By enterbutton =By.id("nav-search-submit-text");
-	//By popup = By.id("sp-cc-accept");
 	
 	
 	By popup = By.id("sp-cc-accept");
-	By bestSeller = By.xpath("//header/div[@id='navbar']/div[@id='nav-main']/div[2]/div[1]/div[1]/a[2]");
+	By bestSeller = By.linkText("Bestsellers");
 	By selectBook = By.partialLinkText("Boeken");
 	By searchTextbox=By.id("twotabsearchtextbox");
 	By selectEnter = By.xpath("//span[@id='nav-search-submit-text']");
-	By selectedBook = By.xpath("//body/div[@id='a-page']/div[@id='search']/div[1]/div[2]/div[1]/span[3]/div[2]/div[1]/div[1]/span[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/span[1]/a[1]/div[1]/img[1]");
+	By selectedBook = By.partialLinkText("Think Like a Monk");
 	By addtoCart = By.id("add-to-cart-button");
 	
 	
